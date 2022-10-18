c83bc407c858722e1f7639acfa515855415582d5
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-10-18 23:13:51 +0200

    Created mocData- tasks list

---
+++ src/mockData.js
@@ -0,0 +1,160 @@
+//completed active blocked
+const mockData = [
+  {
+    id: 0,
+    title: 'Application Setup',
+    status: 'completed',
+    businessContext: [
+      {
+        id: 0,
+        author: 'Alga Nelson',
+        date: 'Dec 17',
+        title: 'Hello 1',
+        content:
+          'Aaaaaa aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+      {
+        id: 1,
+        author: 'Bolga Nelson',
+        date: 'Dec 18',
+        title: 'Hello 2',
+        content:
+          'Bbbbbb aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+      {
+        id: 2,
+        author: 'Colga Nelson',
+        date: 'Dec 19',
+        title: 'Hello 3',
+        content:
+          'Cccccc aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: 'Static Books List',
+    status: 'completed',
+    businessContext: [
+      {
+        id: 0,
+        author: 'Alga Nelson',
+        date: 'Dec 17',
+        title: 'Hello 1',
+        content:
+          'Aaaaaa aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+      {
+        id: 1,
+        author: 'Bolga Nelson',
+        date: 'Dec 18',
+        title: 'Hello 2',
+        content:
+          'Bbbbbb aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+      {
+        id: 2,
+        author: 'Colga Nelson',
+        date: 'Dec 19',
+        title: 'Hello 3',
+        content:
+          'Cccccc aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Administration Panel',
+    status: 'completed',
+    businessContext: [
+      {
+        id: 0,
+        author: 'Alga Nelson',
+        date: 'Dec 17',
+        title: 'Hello 1',
+        content:
+          'Aaaaaa aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+      {
+        id: 1,
+        author: 'Bolga Nelson',
+        date: 'Dec 18',
+        title: 'Hello 2',
+        content:
+          'Bbbbbb aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+      {
+        id: 2,
+        author: 'Colga Nelson',
+        date: 'Dec 19',
+        title: 'Hello 3',
+        content:
+          'Cccccc aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Connect Admin with Frontend',
+    status: 'active',
+    businessContext: [
+      {
+        id: 0,
+        author: 'Alga Nelson',
+        date: 'Dec 17',
+        title: 'Hello 1',
+        content:
+          'Aaaaaa aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+      {
+        id: 1,
+        author: 'Bolga Nelson',
+        date: 'Dec 18',
+        title: 'Hello 2',
+        content:
+          'Bbbbbb aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+      {
+        id: 2,
+        author: 'Colga Nelson',
+        date: 'Dec 19',
+        title: 'Hello 3',
+        content:
+          'Cccccc aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Book review feature',
+    status: 'blocked',
+    businessContext: [
+      {
+        id: 0,
+        author: 'Alga Nelson',
+        date: 'Dec 17',
+        title: 'Hello 1',
+        content:
+          'Aaaaaa aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+      {
+        id: 1,
+        author: 'Bolga Nelson',
+        date: 'Dec 18',
+        title: 'Hello 2',
+        content:
+          'Bbbbbb aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+      {
+        id: 2,
+        author: 'Colga Nelson',
+        date: 'Dec 19',
+        title: 'Hello 3',
+        content:
+          'Cccccc aaaaaaaaaaaaaaa aaaaaaaaaaa aaaaaaaaa aaaaaaa aaaaa aaaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa aaaaaa',
+      },
+    ],
+  },
+];
+
+export default mockData;
