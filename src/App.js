f32dfacbf5512f8c5e11aaa1c42a3614a37d2848
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-10-19 03:47:20 +0200

    Created and styled navbar, styled App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import './App.css';
+import Navbar from './components/layout/Navbar';
+import classes from './App.module.css';
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className={classes.app}>
+      <Navbar />
+      <main>main content</main>
+    </div>
+  );
 }
 
 export default App;
