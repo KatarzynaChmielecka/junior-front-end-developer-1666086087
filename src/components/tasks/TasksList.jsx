266c5510f104efcceaefec7878bf0ca734a9527f
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-10-20 23:19:38 +0200

    Created & styled business context tabs part, refactor TasksList, change date to created_at in mockData

--- src/components/tasks/TasksList.jsx
+++ src/components/tasks/TasksList.jsx
@@ -2,11 +2,22 @@ import { Link } from 'react-router-dom';
 
 import classes from './TasksList.module.css';
 
-const TasksList = ({ srcImage, href, title, alt }) => {
+const TasksList = ({ srcImage, href, title, alt, status }) => {
   return (
-    <div className={classes['tasks-list-wrapper']}>
+    <div
+      className={classes['tasks-list-wrapper']}
+      style={{ color: status === 'blocked' ? '#aeb5c1' : '' }}
+    >
       <img src={srcImage} alt={alt} />
-      <Link to={`${href}`} className={classes['tasks-list-wrapper__task-name']}>
+      <Link
+        to={`${href}`}
+        className={classes['tasks-list-wrapper__task-name']}
+        style={{
+          fontWeight: status === 'active' ? 600 : 500,
+          color: status === 'blocked' && '#aeb5c1',
+          pointerEvents: status === 'blocked' && 'none',
+        }}
+      >
         {title}
       </Link>
     </div>
--- src/pages/TaskTabContent.jsx
+++ src/pages/TaskTabContent.jsx
@@ -1,16 +1,52 @@
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
+import classes from './TaskTabContent.module.css';
 import mockData from '../mockData';
 
 const TaskTabContent = () => {
   const { taskId } = useParams();
-
+  const [read, setRead] = useState(false);
   const filtered = mockData.filter((index) => index.id == taskId);
 
+  const clicked = () => {
+    setRead(true);
+  };
+
   return (
-    <div>
+    <div className={classes['business-context-tabs-wrapper']}>
       {filtered[0].businessContext.map((index) => (
-        <p key={index.id}>{index.author}</p>
+        <div
+          role="button"
+          tabIndex={0}
+          onKeyDown={clicked}
+          key={index.id}
+          onClick={clicked}
+          className={
+            read
+              ? `${classes.read} ${classes['message-tab']}`
+              : `${classes.notRead} ${classes['message-tab']}`
+          }
+        >
+          <div className={classes.header}>
+            {!read && <span className={classes.span}>NEW</span>}
+            <p>{index.author}</p>
+            <span>&#183;</span>
+            <p>
+              {new Date(index.created_at).toLocaleDateString('en', {
+                month: 'short',
+                day: 'numeric',
+              })}
+            </p>
+          </div>
+          <p className={classes['business-context-title-short']}>
+            {index.title}
+          </p>
+
+          <p className={classes['business-context-content-short']}>
+            {index.content}
+          </p>
+        </div>
       ))}
     </div>
   );
