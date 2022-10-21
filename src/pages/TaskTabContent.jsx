575c07d008f94841174dbf3ab6e3cc782627f6b9
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2022-10-21 04:35:29 +0200

    Created and styled business context, added hour to mockData

--- src/pages/TaskTabContent.jsx
+++ src/pages/TaskTabContent.jsx
@@ -1,6 +1,8 @@
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 
+import avatar from '../assets/avatar-message.png';
 import classes from './TaskTabContent.module.css';
 import mockData from '../mockData';
 
@@ -13,42 +15,101 @@ const TaskTabContent = () => {
     setRead(true);
   };
 
-  return (
-    <div className={classes['business-context-tabs-wrapper']}>
-      {filtered[0].businessContext.map((index) => (
-        <div
-          role="button"
-          tabIndex={0}
-          onKeyDown={clicked}
-          key={index.id}
-          onClick={clicked}
-          className={
-            read
-              ? `${classes.read} ${classes['message-tab']}`
-              : `${classes.notRead} ${classes['message-tab']}`
-          }
-        >
-          <div className={classes.header}>
-            {!read && <span className={classes.span}>NEW</span>}
-            <p>{index.author}</p>
-            <span>&#183;</span>
-            <p>
-              {new Date(index.created_at).toLocaleDateString('en', {
-                month: 'short',
-                day: 'numeric',
-              })}
-            </p>
-          </div>
-          <p className={classes['business-context-title-short']}>
-            {index.title}
-          </p>
+  const diffDate = (index) => {
+    return Math.ceil(
+      (new Date() - new Date(index.created_at)) / (1000 * 3600 * 24),
+    );
+  };
 
-          <p className={classes['business-context-content-short']}>
-            {index.content}
-          </p>
-        </div>
-      ))}
-    </div>
+  const day = (index) =>
+    new Date(index.created_at).toLocaleDateString('en', {
+      day: 'numeric',
+    });
+
+  const month = (index) =>
+    new Date(index.created_at).toLocaleDateString('en', {
+      month: 'long',
+    });
+  return (
+    <>
+      <Tabs
+        defaultIndex={1}
+        className={classes['business-context-tabs-wrapper']}
+      >
+        <TabList className={classes['tab-list']}>
+          {filtered[0].businessContext.map((index) => (
+            <Tab
+              key={index.id}
+              onClick={clicked}
+              className={
+                read
+                  ? `${classes.read} ${classes['message-tab']}`
+                  : `${classes.notRead} ${classes['message-tab']}`
+              }
+            >
+              <div className={classes.header}>
+                {!read && <span className={classes.span}>NEW</span>}
+                <p>{index.author}</p>
+                <span>&#183;</span>
+                <p>
+                  {new Date(index.created_at).toLocaleDateString('en', {
+                    month: 'short',
+                    day: 'numeric',
+                  })}
+                </p>
+              </div>
+              <p className={classes['business-context-title-short']}>
+                {index.title}
+              </p>
+
+              <p className={classes['business-context-content-short']}>
+                {index.content}
+              </p>
+            </Tab>
+          ))}
+        </TabList>
+        {filtered[0].businessContext.map((index) => (
+          <TabPanel key={index.id}>
+            <div className={classes['tab-panel-wrapper']}>
+              <p className={classes['tab-panel-wrapper__title']}>
+                {index.title}
+              </p>
+
+              <div className={classes['tab-panel__content-wrapper']}>
+                <img src={avatar} alt="author" />
+                <div>
+                  <div className={classes['tab-panel__content']}>
+                    <p className={classes['tab-panel__content-author']}>
+                      {index.author}
+                    </p>
+                    <span className={classes['tab-panel__content-separator']}>
+                      &#183;
+                    </span>
+                    <p className={classes['tab-panel__content-diff-date']}>
+                      {diffDate(index)} day(s) ago,
+                    </p>
+                    <p className={classes['tab-panel__content-date']}>
+                      <span>{day(index)}</span>
+                      <span>{month(index)}</span>
+                    </p>
+                    <span className={classes['tab-panel__content-separator']}>
+                      &#183;
+                    </span>
+                    <p className={classes['tab-panel__content-hours']}>
+                      {new Date(index.created_at).getHours()}:
+                      {new Date(index.created_at).getMinutes()}
+                    </p>
+                  </div>
+                  <p className={classes['tab-panel__content-message']}>
+                    {index.content}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </TabPanel>
+        ))}
+      </Tabs>
+    </>
   );
 };
 
